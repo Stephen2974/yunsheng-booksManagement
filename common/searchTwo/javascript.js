8eb0f33848a3008724cd7e09394658bd0fa0a029
Stephen2974 <111230876+Stephen2974@users.noreply.github.com>
2023-02-04 10:47:46 +0800

    Add files via upload

---
+++ common/searchTwo/javascript.js
@@ -0,0 +1,16 @@
+function menuSub() {
+    var menu = document.getElementById('div1')
+    if (menu.style.display == 'none') {
+        menu.style.display = 'block'
+    }
+    else
+        menu.style.display = 'none'
+}
+function menuContent() {
+    var content = document.getElementById('libraryMenu')
+    if (content.style.display == 'none') {
+        content.style.display = 'block'
+    }
+    else
+        content.style.display = 'none'
+}
